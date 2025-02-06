6b55f2e184a40839113dd28d1fa44b18bf561bbf
Rajender Reddy <rajender.reddy@ampcus.com>
2025-02-06 15:20:11 +0530

    new template with tailwindcss & materailTailwindcss

--- frontend/src/components/datatables/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import SearchInput from './SearchInput'
-import customStyles from './CustomStyles'
-import CustomLoader from './Spinner'
-import Pagination from './Pagination'
-import ExportButton from './ExportButton'
-export { SearchInput, CustomLoader, customStyles, ExportButton, Pagination }
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,80 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  InformationCircleIcon,
+  ServerStackIcon,
+  RectangleStackIcon,
+  ShoppingBagIcon,
+  ClipboardDocumentCheckIcon,
+  CakeIcon,
+  UserGroupIcon,
+  ChartBarIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
+import { SignIn, SignUp } from "@/pages/auth";
+import { element } from "prop-types";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        icon:< ShoppingBagIcon {...icon}/>,
+        name:"Orders",
+        path:"/orders",
+        element,
+      },
+      {
+        icon:<ClipboardDocumentCheckIcon {...icon}/>,
+        name:"Inventory",
+        path:"/inventory"
+      },
+      {
+        icon:<CakeIcon {...icon}/>,
+        name:"Products",
+        path:"/products"
+      },
+      {
+        icon:<UserGroupIcon {...icon} />,
+        name:"Customers",
+        path:"/customers"
+      },
+      {
+        icon:<ChartBarIcon {...icon}/>,
+        name:"Sales & Reports",
+        path:"/sales"
+      }
+    ],
+  },
+  {
+    title: "auth pages",
+    layout: "auth",
+    pages: [
+      {
+        icon: <ServerStackIcon {...icon} />,
+        name: "sign in",
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        icon: <RectangleStackIcon {...icon} />,
+        name: "sign up",
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+    ],
+  },
+];
+
+export default routes;
