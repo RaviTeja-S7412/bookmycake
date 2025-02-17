6fcd3d8b00d8c4cfe6d610e521a8395f78c5555f
raviteja-ampcus <raviteja.saka@ampcus.com>
2025-02-17 15:59:12 +0530

    updated tailwind

--- frontend/src/components/front/FrontHeader.jsx
+++ frontend/src/components/front/FrontHeader.jsx
@@ -11,6 +11,7 @@ const FrontHeader = () => {
         <meta name="description" content="" />
         <meta name="keywords" content="" />
 
+        <link href="/src/assets/front/css/main.css" rel="stylesheet" />
         <link href="/src/assets/front/img/favicon.png" rel="icon" />
         <link href="/src/assets/front/img/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
@@ -21,7 +22,7 @@ const FrontHeader = () => {
         <link href="/src/assets/front/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
         <link href="/src/assets/front/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
 
-        <link href="/src/assets/front/css/main.css" rel="stylesheet" />
+        
       </div>
       
       <header id="header" className="header d-flex align-items-center sticky-top">
