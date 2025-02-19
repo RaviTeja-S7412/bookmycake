533e152e8eb7ded100fb8c2cb5629924c869a375
raviteja-ampcus <raviteja.saka@ampcus.com>
2025-02-19 16:21:26 +0530

    updated connection

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -4,7 +4,8 @@ var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 function connectToServer(callback) {
