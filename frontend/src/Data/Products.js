b6ee6ddc9fc5c3b0b6e6ae52716fe17a42c1c6d2
raviteja-ampcus <raviteja.saka@ampcus.com>
2025-02-19 18:24:24 +0530

    updated products

--- frontend/src/Data/Products.js
+++ frontend/src/Data/Products.js
@@ -1,79 +1,79 @@
 const Products = [
-     {
-         id: 1,
-         name: "K-pop BTS Square Cake",
-          href:"#",
-         price: 650,
-         imageSrc: "src/assets/products/KPOP-BTS-PHOTO-CAKE.webp",
+    {
+        id: 1,
+        name: "K-pop BTS Square Cake",
+        href: "#",
+        price: 650,
+        imageSrc: "products/1.jpg",
         imageAlt: "KPOP-BTS-PHOTO-CAKE",
-        flavour:""
-     },
-    
-     {
-         id: 2,
-     href:"#",    
-     name: "Elsa Frozen Cake",
-         price: 750,
-      imageAlt:"",
-         "imageSrc": "src/assets/products/ELSAPHOTOBMCCO.avif",
-     flavour:"",
+        flavour: ""
+    },
+
+    {
+        id: 2,
+        href: "#",
+        name: "Elsa Frozen Cake",
+        price: 750,
+        imageAlt: "",
+        "imageSrc": "products/2.jpg",
+        flavour: "",
 
     },
-     {
-         id: 3,
- href:"#",    
-     name: "Photo Cake Square - Yellow",
-         price: 688,
-      imageAlt:"",
-         imageSrc: "src/assets/products/Square-Photo-Cake-Yellow.webp",
-     flavour:""
-     },
-     {
-         d:4,
-     href:"#",    
-     name: "Avengers Cake - Square",
-         price: 800,
-     imageAlt:"" ,   
-     imageSrc: "src/assets/products/Avenger-Square-Cake1.webp",
-     flavour:"",
-     },
-     {
-         id: 5,
-     href:"#",    
-     name: "FC Bayern München Cake",
-         price: 650,
-     imageAlt:"",    
-     imageSrc: "src/assets/products/foot-ball-club-bayern-munchen.webp",
-     flavour:""
-     },
-     {
-         id: 6,
-     href:"#",    
-     name: "Pubg Photocake D1",
-         price: 1200,
-     imageAlt:"",    
-     imageSrc: "src/assets/products/Pub-g-d1.avif",
-     flavour:""
-     },
-     {
-         id: 7,
-     href:"#",    
-     name: "Avengers Cake",
-         price: 688,
-     imageAlt:"",    
-     imageSrc: "src/assets/products/Avengers-Photo-Cakes.webp",
-     flavour:""
-     },
-     {
-         id: 8,
-     href:"#",    
-     name: "Real Madrid CF Cake",
-         price: 800,
-     imageAlt:"",    
-     imageSrc: "src/assets/products/Real-Madrid-CF-1-kg-Rs-1500.webp",
-     flavour:""
-     },
-    ]
+    {
+        id: 3,
+        href: "#",
+        name: "Photo Cake Square - Yellow",
+        price: 688,
+        imageAlt: "",
+        imageSrc: "products/3.jpg",
+        flavour: ""
+    },
+    {
+        d: 4,
+        href: "#",
+        name: "Avengers Cake - Square",
+        price: 800,
+        imageAlt: "",
+        imageSrc: "products/4.jpg",
+        flavour: "",
+    },
+    {
+        id: 5,
+        href: "#",
+        name: "FC Bayern München Cake",
+        price: 650,
+        imageAlt: "",
+        imageSrc: "products/5.jpg",
+        flavour: ""
+    },
+    {
+        id: 6,
+        href: "#",
+        name: "Pubg Photocake D1",
+        price: 1200,
+        imageAlt: "",
+        imageSrc: "products/6.jpg",
+        flavour: ""
+    },
+    {
+        id: 7,
+        href: "#",
+        name: "Avengers Cake",
+        price: 688,
+        imageAlt: "",
+        imageSrc: "products/7.jpg",
+        flavour: ""
+    },
+    {
+        id: 8,
+        href: "#",
+        name: "Real Madrid CF Cake",
+        price: 800,
+        imageAlt: "",
+        imageSrc: "products/8.jpg",
+        flavour: ""
+    },
+]
 //     {
 //         id: 1,
 //         name: 'Basic Tee',
